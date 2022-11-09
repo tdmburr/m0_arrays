2002f8f08619a1bdb912ce573d1d6b20d755ba45
Trey Marshall <marshall.trey92@yahoo.com>
2022-11-09 16:31:39 -0600

    Add variables

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var cats = ["Ryder", "White", "Ciipaya", "Berger"]
+var numbers = [0, 1, 2, 3]
+var working = true
+var sitting = true
+var standing = false
+var breathing = true
+var actions = [working, sitting, standing, breathing]
