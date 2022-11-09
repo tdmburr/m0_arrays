decf93782e5d0c05c01c879de859d6ae0c0b3c05
Trey Marshall <marshall.trey92@yahoo.com>
2022-11-09 16:33:23 -0600

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,7 +1,14 @@
 var cats = ["Ryder", "White", "Ciipaya", "Berger"]
+//.push will add Carson and Moxie to the end of the existing array designated to cats.
+cats.push("Carson", "Moxie")
+console.log(cats)
 var numbers = [0, 1, 2, 3]
+//.slice will start from the first element number and end at the selected element but won't include the element ended on. It will instead include the numbers leading up to that element as an output. This should result in the numbers 1 and 2 being displayed.
+console.log(numbers.slice(1,3))
 var working = true
 var sitting = true
 var standing = false
 var breathing = true
 var actions = [working, sitting, standing, breathing]
+//.pop will remove the final element in the actions array and provide the boolean response to the question. Breathing? True.
+console.log(actions.pop())
